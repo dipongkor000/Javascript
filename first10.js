7d0b0e8bd03450a7e8c5c388e9e67fca3681270d
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-10 00:12:57 +0600

    git test

--- first10.js
+++ first10.js
@@ -26,25 +26,54 @@
 // }
 
 
-let score = 75;
+// let score = 75;
+// if(score >= 90 && score <=100){
+//     let grade = 'A';
+//     console.log('your grade =', grade);
+// }
+// else if(score >= 80 && score <= 89){
+//     let grade = 'B';
+//     console.log('your grade =', grade);
+// }
+// else if(score >=70 && score <=79){
+//     let grade = 'C';
+//     console.log('your grade =', grade);
+// }
+// else if(score >=60 && score <=69){
+//     let grade = 'D';
+//     console.log('your grade =', grade);
+// }
+// else{
+//     let grade = 'F';
+//     console.log('your grade =', grade);
+// }
 
-if(score >= 90 && score <=100){
-    let grade = 'A';
-    console.log('your grade =', grade)
-}
-else if(score >= 80 && score <= 89){
-    let grade = 'B';
-    console.log('your grade =', grade);
-}
-else if(score >=70 && score <=79){
-    let grade = 'C';
-    console.log('your grade =', grade);
-}
-else if(score >=60 && score <=69){
-    let grade = 'D';
-    console.log('your grade =', grade);
-}
-else{
-    let grade = 'F';
-    console.log('your grade =', grade);
-}
+//alternative rule
+
+// let score = 70;
+// let grade;
+// if(score >= 90 && score <= 100){
+//     grade = "A++";
+// }
+// else if(score >= 80 && score <= 89){
+//     grade = "A+";
+// }
+// else if(score >= 70 && score <= 79){
+//     grade = "A";
+// }
+// else if(score >= 60 && score <= 69){
+//     grade = "B";
+// }
+// else if(score >= 50 && score <= 59){
+//     grade = "C";
+// }
+// else if(score >= 40 && score <= 49){
+//     grade = "D";
+// }
+// else if(score >= 33 && score <= 39){
+//     grade = "E";
+// }
+// else if(score <= 32){
+//     grade = "F";
+// }
+// console.log("according to your score, your grade was :", grade);
