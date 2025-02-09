7a9fdf1832990300bcb6d076898ba08111f758e7
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-09 22:43:57 +0600

    push some edit

--- first10.js
+++ first10.js
@@ -8,20 +8,22 @@
 //     console.log('payment =', coke);
 // }
 
-let weight = 50;
-let height = 1.7;
-let heightSquare = Math.pow(height, 2);
-let BMI = weight / heightSquare;
-if(BMI <= 18.5 ){
-    console.log("you are underweight, Your BMI =", BMI.toFixed(3));
-}
-else if(BMI <= 24.9){
-    console.log('you are normal, Your BMI =', BMI.toFixed(3));
-}
-else if(BMI <= 29.9){
-    console.log('you are overweight, Your BMI =', BMI.toFixed(3));
-}
-else{
-    console.log(BMI.toFixed(3));
-}
+// let weight = 50;
+// let height = 1.7;
+// let heightSquare = Math.pow(height, 2);
+// let BMI = weight / heightSquare;
+// if(BMI <= 18.5 ){
+//     console.log("you are underweight, Your BMI =", BMI.toFixed(3));
+// }
+// else if(BMI <= 24.9){
+//     console.log('you are normal, Your BMI =', BMI.toFixed(3));
+// }
+// else if(BMI <= 29.9){
+//     console.log('you are overweight, Your BMI =', BMI.toFixed(3));
+// }
+// else{
+//     console.log(BMI.toFixed(3));
+// }
 
+
+ 
