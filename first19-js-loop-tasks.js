c84742326d2c8b3d2bce3a42b99c5fea05a942ee
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-12 13:08:39 +0600

    file name change and some text add

---
+++ first19-js-loop-tasks.js
@@ -0,0 +1,50 @@
+// for(i = 61; i <= 100; i++){
+//     if(i % 2 === 1){
+//         console.log('odd numbers =', i);
+//     }
+//     else{
+//         console.log('even numbers =', i);
+//     }
+// }
+// //alternative type
+
+// let i = 61;
+// while(i <= 100){
+//     if(i % 2 ===1){
+//         console.log('odd numbers =', i);
+//     }
+//     else{
+//         console.log('even numbers =', i );
+//     }
+//     i++;
+// }
+
+// for(let i = 78; i <= 98; i++){
+//     if(i % 2 === 0){
+//         console.log('this is even number =',i);
+//     }else{
+//         console.log('this is odd number =',i);
+//     }
+// }
+
+// let s = 0;
+// let d = 81;
+// while(d <= 131){
+//     if(d % 2 === 1){
+//         s = s + d;
+//         console.log(s);
+//     }
+//     d++;
+// } 
+
+
+// let d = 0;
+// for(let s = 206; s <= 311; s++){
+//     d = d + s;
+//     console.log(d);
+// };
+
+// for(let s = 1; s <= 10; s++){
+//     let result = 5 * s;
+//     console.log(`5 * ${s} = ${result}`);
+// }
