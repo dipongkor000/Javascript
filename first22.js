d1207124f91d767ab121ff68d3632553044aa929
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-16 02:52:31 +0600

    add file

---
+++ first22.js
@@ -0,0 +1,40 @@
+// type-1-->
+// Write a function to convert temperature from Celsius to Fahrenheit.?
+// function celsiusToFahrenheit(celsius){
+//     return ((9 * celsius) / 5) + 32;
+// }
+// console.log(celsiusToFahrenheit(44));
+
+//type-2-->
+// You are given an array of numbers. Count how many times the a number is repeated in the array.?
+// function findArrayNum(find,arrayNumbers){
+//     let div = [];
+//     for(const s of arrayNumbers){
+//         if(find === s){
+//             div.push(s);
+//         }
+//     }
+//     return div.length;
+// }
+// let array = [5,6,11,12,98,5];
+// console.log(findArrayNum(5,array));
+
+// type-3-->
+// Write a function to count the number of vowels in a string.?
+// function vowelsNum(word){
+//     let div = [];
+//     for(const s of word){
+//         if(s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u'){
+//             div.push(s);
+//         }
+//     }
+//     return div.length;
+// }
+// console.log(vowelsNum('dipongkorroy'));
+
+//type-4--->
+// Write a function to find the longest word in a given string.?
+function largeWord(sentence){
+    
+}
+console.log(largeWord('I am learning programming to become a programmer'));
