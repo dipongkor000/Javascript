41d90fc4f02bc9e8f40f5f9359121e2ec64b994e
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-16 13:12:14 +0600

    add file

--- first22.js
+++ first22.js
@@ -32,9 +32,38 @@
 // }
 // console.log(vowelsNum('dipongkorroy'));
 
+// type-3.2-->
+//Does a word have all the vowels?
+// const word = "a e i o u";
+// const vowels = ["a", "e", "i", "o", "u"];
+// let isAllVowelsPresent = true;
+
+// for (let s of vowels) {
+//   if (word.toLowerCase().includes(s) == false) {
+//     isAllVowelsPresent = false;
+//     break;
+//   }
+// }
+
+// console.log(isAllVowelsPresent ? "ALL OK" : "NOT OK");
+
 //type-4--->
 // Write a function to find the longest word in a given string.?
-function largeWord(sentence){
-    
-}
-console.log(largeWord('I am learning programming to become a programmer'));
+// function largeWord(sentence){
+//     let array = sentence.split(' ');
+//     let div = array[0];
+//     for(let w of array){
+//         if(array[0].length < w.length){
+//             divStringLength = w.length;
+//             div = w;
+//         }
+//     }
+//     return div;
+// }
+// console.log(largeWord('I am learning programming to become a programmer'));
+
+// type-5-->
+console.log(Math.round(Math.random()*11 + 10));
+
+
+
